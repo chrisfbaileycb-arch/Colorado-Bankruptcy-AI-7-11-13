a74a6c264e1103a2cf8d4cd42fb3450a177772ff
chrisfbaileycb-arch <chrisfbailey.cb@gmail.com>
2026-08-03 23:28:22 -0600

    feat: Push core lib index and master case types schema to GitHub

--- lib/index.ts
+++ lib/index.ts
@@ -1,18 +1,6 @@
-import { mapMasterCaseToForm101Fields } from './mappers/form-101';
-import { mapMasterCaseToForm121Fields } from './mappers/form-121';
-import { mapForm106AB } from './mappers/form-106ab';
-import { mapForm106C } from './mappers/form-106c';
-import { mapForm106D } from './mappers/form-106d';
-import { mapForm106EF } from './mappers/form-106ef';
-import { mapForm106G } from './mappers/form-106g';
-import { mapForm106H } from './mappers/form-106h';
-import { mapForm106I } from './mappers/form-106i';
-import { mapForm106J } from './mappers/form-106j';
-import { mapForm106J2 } from './mappers/form-106j2';
-import { mapForm107 } from './mappers/form-107';
-import { mapForm108 } from './mappers/form-108';
-import { mapMasterCaseToForm122A1 } from './mappers/form-122a1';
-import { mapMasterCaseToForm122A2 } from './mappers/form-122a2';
+import type { MasterCaseData } from './types/master-case';
+import { getJurisdictionPack } from './jurisdictions';
 
 export * from './types/master-case';
+export * from './jurisdictions';
 export * from './engine';
